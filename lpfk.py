59958fb940d8a012f841b7626f05fb8fb4cc8157
Daniel M. Drucker <dmd@3e.org>
2016-05-11 22:53:36 +0000

    moved toggler to new file

--- lpfk.py
+++ lpfk.py
@@ -77,26 +77,4 @@ class lpfk:
     def close(self):
         self.sp.close()
 
-
-def toggler():
-    lp = lpfk()
-    
-    while True:
-        cmd = lp.get()
-        if not len(cmd):
-            continue
-        if cmd == lp.RETRANSMIT:
-            print ('got error, resending')
-            update_lights()
-            continue
-        cmd = ord(cmd)
-
-        lp.keys[cmd] ^= 1
-        print('button {} set to {}'.format(cmd, lp.keys[cmd]))
-        lp.update_lights()
-
-
-
-if __name__ == '__main__':
-    toggler()
     
---
+++ toggler.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from lpfk import lpfk
+
+def toggler():
+    lp = lpfk()
+    
+    while True:
+        cmd = lp.get()
+        if not len(cmd):
+            continue
+        if cmd == lp.RETRANSMIT:
+            print ('got error, resending')
+            update_lights()
+            continue
+        cmd = ord(cmd)
+
+        lp.keys[cmd] ^= 1
+        print('button {} set to {}'.format(cmd, lp.keys[cmd]))
+        lp.update_lights()
+
+
+if __name__ == '__main__':
+    toggler()
