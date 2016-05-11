a2417be0581d0b1aa0ea1e68779a01f38138d22c
Daniel M. Drucker <dmd@3e.org>
2016-05-11 22:51:09 +0000

    class-ified

--- lpfk.py
+++ lpfk.py
@@ -6,76 +6,97 @@ import serial
 from time import sleep
 import struct
 
-PORT = os.getenv('LIGHTS_PORT', '/dev/serial/by-id/usb-Silicon_Labs_IBM-LPFK_75BB67F3-A9AA8F7A-if00-port0')
+
+class lpfk:
+    PORT = os.getenv('LIGHTS_PORT', '/dev/serial/by-id/usb-Silicon_Labs_IBM-LPFK_75BB67F3-A9AA8F7A-if00-port0')
+
+    RESET = b'\x01'
+    READCONF = b'\x06'
+    CONFOK = b'\x03'
+    ENABLE = b'\x08'
+    SET = b'\x94'
+    RETRANSMIT = b'\x80'
+    OK = b'\x81'
+
+    def __init__(self):
+        self.keys = [0] * 32
+        self.sp = serial.Serial(port=self.PORT,
+                           baudrate=9600,
+                           bytesize=serial.EIGHTBITS,
+                           parity=serial.PARITY_ODD,
+                           stopbits=serial.STOPBITS_ONE,
+                           xonxoff=False,
+                           rtscts=False,
+                           dsrdtr=False)
+
+        # reset
+        self.sp.timeout = 1
+        self.get()
+        self.sp.timeout = 10
+        self.sp.write(self.RESET)
+        sleep(0.8)
+
+        # check ID
+        self.sp.write(self.READCONF)
+
+        if self.get() == self.CONFOK:
+            print ("LPFK detected")
+        else:
+            print ("uh oh, LPFK not detected")
+            self.sp.close()
+            sys.exit(1)
+
+        # enable
+        self.sp.write(b'\x08')
+        sleep(0.1)
 
 
-##### setup
+    def update_lights(self, retries=0):
+        self.sp.write(self.SET)
+        self.sp.write(struct.pack('BBBB',
+                             int(''.join(str(k) for k in self.keys[0:8]),2),
+                             int(''.join(str(k) for k in self.keys[8:16]),2),
+                             int(''.join(str(k) for k in self.keys[16:24]),2),
+                             int(''.join(str(k) for k in self.keys[24:32]),2)))
+        resp = self.get()
+        if resp == b'\x80':
+            print('error, retry')
+            if retries > 2:
+                print('{} retries and still failing so aborting'.format(retries))
+                sys.exit(1)
+            else:
+                self.update_lights(retries+1)
+        elif resp == b'\x81':
+            pass # print('good set')
+        else:
+            print('huh??')
 
-sp = serial.Serial(port=PORT,
-                   baudrate=9600,
-                   bytesize=serial.EIGHTBITS,
-                   parity=serial.PARITY_ODD,
-                   stopbits=serial.STOPBITS_ONE,
-                   xonxoff=False,
-                   rtscts=False,
-                   dsrdtr=False)
+    def get(self):
+        return self.sp.read(1)
 
-# reset
-sp.timeout = 1
-sp.read(1)
-sp.timeout = 10
-sp.write(b'\x01')
-sleep(0.8)
+    def close(self):
+        self.sp.close()
 
-# check ID
-sp.write(b'\x06')
-resp = sp.read(1)
 
-if resp == b'\x03':
-    print ("LPFK detected")
-else:
-    print ("uh oh, LPFK not detected")
-    sys.exit(1)
-
-# enable
-sp.write(b'\x08')
-sleep(0.1)
-
-##### control
-
-status = [0] * 32
-
-def update_lights(status):
-    sp.write(b'\x94')
-    sp.write(struct.pack('BBBB',
-                         int(''.join(str(k) for k in status[0:8]),2),
-                         int(''.join(str(k) for k in status[8:16]),2),
-                         int(''.join(str(k) for k in status[16:24]),2),
-                         int(''.join(str(k) for k in status[24:32]),2)))
-    resp = sp.read(1)
-    if resp == b'\x80':
-        print('error')
-    elif resp == b'\x81':
-        pass # print('good set')
-    else:
-        print('huh??')
-
-while True:
-    cmd = sp.read(1)
-    if not len(cmd):
-        next
-    cmd = ord(cmd)
-    if cmd == 0x80:
-        print ('got error, resending')
-        update_lights(status)
-        next
-
-    status[cmd] ^= 1
-    print('button {} set to {}'.format(cmd, status[cmd]))
-    update_lights(status)
+def toggler():
+    lp = lpfk()
     
-sp.close()
+    while True:
+        cmd = lp.get()
+        if not len(cmd):
+            continue
+        if cmd == lp.RETRANSMIT:
+            print ('got error, resending')
+            update_lights()
+            continue
+        cmd = ord(cmd)
+
+        lp.keys[cmd] ^= 1
+        print('button {} set to {}'.format(cmd, lp.keys[cmd]))
+        lp.update_lights()
 
 
 
-
+if __name__ == '__main__':
+    toggler()
+    
